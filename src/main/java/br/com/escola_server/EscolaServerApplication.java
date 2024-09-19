362e63d89731418347511fde5a6cb4506cdbc50b
Dogulas Dreer <douglasdreer@outlook.com.br>
2024-09-18 23:27:13 -0300

    Commit initial

---
+++ src/main/java/br/com/escola_server/EscolaServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.escola_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscolaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscolaServerApplication.class, args);
+	}
+
+}
