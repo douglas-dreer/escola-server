febb3bfc427f10180246cb74ce8c5e5f34e72b89
Dogulas Dreer <douglasdreer@outlook.com.br>
2024-09-25 10:47:53 -0300

    [ FIX ] Refactored in two line for simplify understand

--- src/main/java/br/com/escola_server/utilitaries/DTOGenerator.java
+++ src/main/java/br/com/escola_server/utilitaries/DTOGenerator.java
@@ -1,16 +1,20 @@
 package br.com.escola_server.utilitaries;
 
+import br.com.escola_server.entities.Address;
 import br.com.escola_server.entities.Contact;
 import br.com.escola_server.entities.Person;
 import br.com.escola_server.enums.ContactType;
+import br.com.escola_server.models.AddressDTO;
 import br.com.escola_server.models.ContactDTO;
 import br.com.escola_server.models.PersonDTO;
+import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.UUID;
 
+@Slf4j
 public class DTOGenerator {
     public PersonDTO createPersonDTO() {
         ContactDTO contactDTO = createContactDTO();
@@ -27,7 +31,8 @@ public class DTOGenerator {
     }
 
     public Person createPerson() {
-        return Converter.convertTo(createPersonDTO(), Person.class);
+        PersonDTO personDTO = createPersonDTO();
+        return Converter.convertTo(personDTO, Person.class);
     }
 
     public ContactDTO createContactDTO() {
@@ -43,7 +48,29 @@ public class DTOGenerator {
     }
 
     public Contact createContact() {
-        return Converter.convertTo(createContactDTO(), Contact.class);
+        ContactDTO contactDTO = createContactDTO();
+        return Converter.convertTo(contactDTO, Contact.class);
+    }
+
+    public AddressDTO createAddressDTO() {
+        return AddressDTO.builder()
+                .id(UUID.randomUUID())
+                .street("Street")
+                .number(1)
+                .complement("Complement")
+                .neighborhood("Neighborhood")
+                .postalCode("12345678")
+                .city("City")
+                .state("State")
+                .createdAt(LocalDateTime.now().minusWeeks(1))
+                .updatedAt(LocalDateTime.now())
+                .isMain(true)
+                .build();
+    }
+
+    public Address createAddress() {
+        AddressDTO addressDTO = createAddressDTO();
+        return Converter.convertTo(addressDTO, Address.class);
     }
 
 
